9efc8e3480d68e975dad1e87f559f52ba916e49e
Elvin Ega <proega3@gmail.com>
2017-10-05 23:29:11 +0300

    initial

---
+++ users/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url, patterns
+
+from .views import *
+
+urlpatterns = [
+	url(r'^login/$', login),
+	url(r'^create_user/$', create_user),
+	url(r'^approve_users/$', approve_users),
+	url(r'^view_users/(?P<state>[a-z]+)/$', view_users),
+	url(r'^delete_user/(?P<user_id>[0-9a-z-]+)/$', delete_user),
+	url(r'^check_activation_key/(?P<key>[A-Za-z0-9]+)/$',check_activation_key),
+	url(r'^activate_account/$', activate_account)
+]
